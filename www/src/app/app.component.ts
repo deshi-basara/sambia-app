b12b4d5fa2a2aa45785192f28c3516a7fabf4222
deshi-basara <simon@i-was-perfect.net>
2016-06-20 23:15:20 +0200

    Material Library and navigation added

--- www/src/app/app.component.ts
+++ www/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [
+    MD_SIDENAV_DIRECTIVES,
+  ],
 })
 export class AppComponent {
   title = 'app works!';
--- www/src/system-config.ts
+++ www/src/system-config.ts
@@ -7,11 +7,33 @@
  **********************************************************************************************/
 /** Map relative paths to URLs. */
 const map: any = {
+  '@angular2-material': 'vendor/@angular2-material'
 };
 
 /** User packages configuration. */
 const packages: any = {
 };
+
+// material packages
+const materialPkgs:string[] = [
+  'core',
+  'button',
+  'card',
+  'checkbox',
+  'icon',
+  'input',
+  'list',
+  'progress-bar',
+  'progress-circle',
+  'radio',
+  'sidenav',
+  'slide-toggle',
+  'tabs',
+  'toolbar',
+];
+materialPkgs.forEach((pkg) => {
+  packages[`@angular2-material/${pkg}`] = {main: `${pkg}.js`};
+});
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 /***********************************************************************************************
