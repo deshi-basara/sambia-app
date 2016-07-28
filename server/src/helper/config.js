e863017a77df88dca6bbf9bd19958bdff7f9baa6
deshi-basara <simon@i-was-perfect.net>
2016-07-28 12:06:00 +0200

    Code alignment fix

--- server/src/helper/config.js
+++ server/src/helper/config.js
@@ -3,13 +3,13 @@ import development from '../config/development';
 
 let config;
 switch (process.env.NODE_ENV) {
-    case 'production':
-        config = production;
-        break;
-    case 'development':
-    default:
-        config = development;
-        break;
+  case 'production':
+    config = production;
+    break;
+  case 'development':
+  default:
+    config = development;
+    break;
 }
 
 export default config;
