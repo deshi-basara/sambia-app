32e385df1367eed73e57f84f1e36aa9a6055be0c
deshi-basara <simon@i-was-perfect.net>
2016-07-28 15:37:59 +0200

    Build tasks added

--- server/gulpfile.babel.js
+++ server/gulpfile.babel.js
@@ -1,10 +1,18 @@
+import beep from 'beepbeep';
+import del from 'del';
+
 import gulp from 'gulp';
 import babel from 'gulp-babel';
-import eslint from 'gulp-esling';
+import flow from 'gulp-flowtype';
+import eslint from 'gulp-eslint';
+import nodemon from 'gulp-nodemon';
 import Cache from 'gulp-file-cache';
 
+import notifier from 'node-notifier';
+import runSequence from 'run-sequence';
+
 /**
- * Gulp configuration
+ * Global conf
  */
 const config = {
   beep: true, // make beep-sound on errors
@@ -18,12 +26,94 @@ const config = {
 };
 
 /**
+ * Run flow type-checking and print errors.
+ */
+gulp.task('types', () =>
+  gulp.src(`${config.path.src}/**/*.js`)
+    .pipe(flow({
+      all: false,
+      weak: false,
+      killFlow: false,
+      beep: config.beep,
+      abort: false,
+    }))
+);
+
+/**
+ * Run javascript linter and print errors.
+ */
+gulp.task('linter', () =>
+  gulp.src(`${config.path.src}/**/*.js`)
+    .pipe(eslint())
+    .pipe(eslint.formatEach())
+    .pipe(eslint.result((results) => {
+      // beep, if errors were found
+      if (results.errorCount > 0 && config.beep) {
+        beep();
+      }
+    }))
+);
+
+/**
  * Compile ES2015 code with babel and cache compiled files.
  */
-gulp.task('compile', () => {
-  return gulp.src(`${config.path.src}/**/*.js`)
+gulp.task('compile:tmp', () =>
+  gulp.src(`${config.path.src}/**/*.js`)
     .pipe(config.cache.filter()) // remember files
     .pipe(babel()) // compile
     .pipe(config.cache.cache()) // cache compiled files
-    .pipe(gulp.dest(config.path.tmp));
+    .pipe(gulp.dest(config.path.tmp))
+);
+
+/**
+ * Compile ES2015 code with babel
+ */
+gulp.task('compile:dist', () =>
+  gulp.src(`${config.path.src}/**/*.js`)
+    .pipe(babel())
+    .pipe(gulp.dest(config.path.dist))
+);
+
+/**
+ * Runs all tasks synchronously.
+ */
+gulp.task('runSync', done => {
+  // execute synchronously on changes
+  runSequence(
+    'linter',
+    'types',
+    'compile:tmp',
+    done
+  );
 });
+
+/**
+ * Restarts the node-app after changes were applied.
+ */
+gulp.task('serve', ['compile'], () => {
+  nodemon({
+    script: `${config.path.tmp}/app.js`,
+    watch: config.path.src,
+    tasks: ['runSync'],
+  })
+  .on('restart', () => {
+    notifier.notify({ title: 'Serve', message: 'Server restarted' });
+  });
+});
+
+/**
+* Runs all tasks synchronously.
+*/
+gulp.task('build', done =>
+  // execute synchronously
+  runSequence(
+    'linter',
+//    'types',
+    'clean',
+    'compile:dist',
+    done
+  )
+);
+
+// empty dist folder, before creating a new build
+gulp.task('clean', () => del([config.path.dist], { dot: true }));
