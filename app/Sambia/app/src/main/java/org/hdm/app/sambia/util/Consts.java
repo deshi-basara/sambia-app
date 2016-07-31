2b0d8ec3b8f68c57917e44271835371892c0b828
deshi-basara <simon@i-was-perfect.net>
2016-07-31 19:14:01 +0200

    Sync data with server

--- app/Sambia/app/src/main/java/org/hdm/app/sambia/util/Consts.java
+++ app/Sambia/app/src/main/java/org/hdm/app/sambia/util/Consts.java
@@ -12,17 +12,19 @@ public final class Consts {
 
     public final static String JSONFILE = "activitys.json";
     public final static String TEMPACTIVITIES = "temp-activities.json";
+    public final static String PARENTPATH = "SambiaApp";
+    public final static String IMAGEPATH = "Images";
+    public final static String CONFIGPATH = "Config";
+    public final static String LOGPATH = "Logs";
 
     public final static String ACTIVITIES = "activitys";
 
+    public final static String MAINFOLDER = "mainFolder";
+    public final static String IMAGEFOLDER = "imageFolder";
+    public final static String CONFIGFOLDER = "configFolder";
+    public final static String LOGFOLDER = "logFolder";
 
-    public final static String MAINFOLDER = "SambiaApp";
-    public final static String IMAGEFOLDER = "Images";
-    public final static String CONFIGFOLDER = "Config";
-    public final static String LOGFOLDER = "Logs";
-
-
-    public final static String PROPERTIESFILE = "folder.properties";
+        public final static String PROPERTIESFILE = "folder.properties";
 
 
 
